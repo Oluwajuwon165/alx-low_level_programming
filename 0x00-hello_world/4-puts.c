448d116f216f47ccb328da37ec68212548d08c52
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2022-06-30 16:28:19 -0700

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-#!/bin/bash
-gcc -o cisfun $CFILE
- 
-task 4
 #include <stdio.h>
 #include <string.h>
  
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
  
 int main(void)
 {
