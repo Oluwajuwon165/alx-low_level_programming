62ded8f49e879dc0df7ce5d687c7dfaf1d3af7b4
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2022-06-30 16:15:25 -0700

    t4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#!/bin/bash
+gcc -o cisfun $CFILE
+ 
+task 4
+#include <stdio.h>
+#include <string.h>
+ 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+ 
+return (0);
+}
