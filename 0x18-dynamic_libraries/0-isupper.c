3b9e11a09b2550d9e5233564a20ced175745aa1c
Oluwajuwon165 <99687642+Oluwajuwon165@users.noreply.github.com>
2022-10-04 07:19:45 +0100

    Update

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-
 /**
- * _isupper - checks for uppercase
- * @c: character to checked for case
- *
- * Return: 1 if uppercase, 0 if not uppercase
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	return (0);
 }
